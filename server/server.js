e30be2a737c9ae0fc4cceb6bc2c597eb41d749d7
parthbarot9708 <parth.barot9708@gmail.com>
2025-01-30 13:23:01 -0500

    Added backend setup with Express and MongoDB

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+// Load environment variables
+dotenv.config();
+
+// Connect to MongoDB
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Sample Route
+app.get("/", (req, res) => {
+    res.send("API is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
